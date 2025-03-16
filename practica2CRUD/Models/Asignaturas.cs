162206783df6bdacc0f85f7edf6f0f577bbbb7f9
Keiry <keyri2017hernadez@gmail.com>
2025-03-16 15:55:47 -0600

    Primer commit

---
+++ practica2CRUD/Models/Asignaturas.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace practica2CRUD.Models
+{
+    public class Asignaturas
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Nombre { get; set; } = string.Empty;
+        [StringLength(200)]
+        public string? Descripcion { get; set; }
+    }
+}
